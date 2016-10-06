75e3bf0d9f3d6e63ceeff4edd62ea2422bfd3fcd
John Leith <jleith@velocitywebworks.com>
2016-10-06 12:43:22 -0600

    disabling listeners temporarily

--- binding/listeners.py
+++ binding/listeners.py
@@ -16,15 +16,15 @@ def get_bindings(model):
     return Binding.bindings.pattern(model.__name__ + ":*") or []
 
 
-def model_saved(sender=None, instance=None, **kwargs):
-    for binding in get_bindings(sender):
-        binding.model_saved(sender=sender, instance=instance, **kwargs)
-        # print("{}:{} saved".format(sender, instance), binding)
-post_save.connect(model_saved, dispatch_uid="binding-post-save")
-
-
-def model_deleted(sender=None, instance=None, **kwargs):
-    for binding in get_bindings(sender):
-        binding.model_deleted(sender=sender, instance=instance, **kwargs)
-        # print("{}:{} deleted".format(sender.__name__, instance), binding)
-post_delete.connect(model_deleted, dispatch_uid="binding-post-delete")
+# def model_saved(sender=None, instance=None, **kwargs):
+#     for binding in get_bindings(sender):
+#         binding.model_saved(sender=sender, instance=instance, **kwargs)
+#         # print("{}:{} saved".format(sender, instance), binding)
+# post_save.connect(model_saved, dispatch_uid="binding-post-save")
+#
+#
+# def model_deleted(sender=None, instance=None, **kwargs):
+#     for binding in get_bindings(sender):
+#         binding.model_deleted(sender=sender, instance=instance, **kwargs)
+#         # print("{}:{} deleted".format(sender.__name__, instance), binding)
+# post_delete.connect(model_deleted, dispatch_uid="binding-post-delete")
