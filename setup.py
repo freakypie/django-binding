bbd0ce5d1c4348e3c6afa23657758560efa26861
John Leith <jleith@velocitywebworks.com>
2018-02-07 17:25:00 -0700

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-binding',
-    version='0.6.19',
+    version='0.6.20',
     description='Django data binding backend',
     author='John Leith',
     author_email='dev@velocitywebworks.com',
